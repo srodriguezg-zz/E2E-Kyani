1dc893c5d58efdc40bc8fd4559f9cf3882860d6c
Santiago Rodriguez Gutierrez <srodriguezg@pslcol.com.co>
2018-08-01 14:10:45 -0500

    Add awaiters

--- test/Google.spec.ts
+++ test/Google.spec.ts
@@ -1,13 +1,16 @@
-import { browser } from 'protractor';
+import { browser, $, protractor } from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
-  describe('when open Google Page', () => {
+  describe('when open Google Page', () => {    
     beforeEach(() => {
       browser.get('http://www.google.com');
     });
 
-    it('then should have a title', () => {
+    it('then should have a title', async () => {
       expect(browser.getTitle()).toEqual('Google');
+      var searchTextBox = $("#lst-ib");
+      await searchTextBox.sendKeys("Hola");
+      await browser.actions().sendKeys(protractor.Key.ENTER).perform();
     });
   });
 });
